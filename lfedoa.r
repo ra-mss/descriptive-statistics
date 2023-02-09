c0d2ed90eea765862bc603dbad92093c635a718f
Ram <121362638+ra-mss@users.noreply.github.com>
2023-02-08 20:38:58 -0600

    Computes descriptive measures of the variables

--- lfedoa.r
+++ lfedoa.r
@@ -4,4 +4,14 @@ library(fdth)
 library(modeest)
 library(xlsx)
 
-base <- read.excel('../home/ra_mss/Desktop/R_Projects/LIMOSAFEDOA.xlsx')
+base <- read_excel('../ra_mss/Desktop/R_Projects/LIMOSAFEDOA/BASE_LIMOSA.xlsx')
+head(base)
+
+#Medidas descriptivas de variables cuantitativas en L. fedoa
+statistics = matrix(c(summarise_all(base, mean, na.rm=TRUE), 
+                      summarise_all(base, median, na.rm=TRUE),
+                      summarise_all(base, mfv1, na.rm=TRUE),
+                      summarise_all(base, range, na.rm=TRUE),
+                      summarise_all(base, var, na.rm=TRUE),
+                      summarise_all(base, sd, na.rm=TRUE)),
+                    ncol = 6)
